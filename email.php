5fbef11adb674a7909b249e80beb2b2a0fa799db
Owen King <Owen King@DESKTOP-204TB8O>
2018-11-25 19:10:08 -0500

    chas

--- email.php
+++ email.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST["submit"]))
     {
-        $to = "oek8462@g.rit.edu";
+        $to = "oek8462@rit.edu";
         
         $from = empty(trim($_POST["recipient-email"])) ? "noemail@sample.com" : sanitize_string($_POST["recipient-email"]);
 
@@ -10,12 +10,14 @@
         $message = empty(trim($_POST["message-text"])) ?  "No message" : sanitize_string($_POST["message-text"]);
 
         $name = empty(trim($_POST["recipient-name"])) ? "No name" : sanitize_string($_POST["recipient-name"]);
+        
+        $human = empty(trim($_POST["human"])) ? "0" : sanitize_string($_POST["human"]);
+
+        $headers = "From: $from" . "\r\n";
+        if (intval($human) == 4){
 
         $sent = mail($to,$subject,$message,$headers);
-        if ($sent){
-            echo "<p><b>You sent:</b> $message</p>";
-        }else{
-            echo "<p>Mail not sent!</p>";
         }
+        
     }
     ?>
